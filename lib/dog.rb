e950e0dd76b169189b1753e7ec202b7c672a90a1
Agnes Park <agnespark824@gmail.com>
2020-02-02 19:40:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,25 @@
 class Dog
   attr_accessor "name:", 
   
+  @@all = []
+  
   def initialize(name)
     @name = name
-    @all
+    save
+  end
+  
+  def all
+    @@all
+  end
+  
+  def print_all
+    @@all.each do |index|
+      puts index
+    end
+  end
+  
+  def save
+    @@all << @name
   end
   
 end
